ada7424899bc9471d3d24a2c9ed924d2b80ca170
steveyalcin <steven.yalcin@gmail.com>
2020-01-15 20:03:54 -0400

    Counter component is removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Counter from './featureComponents/Counter';
 import People from './featureComponents/People';
 
 const App = () => {
   return (
     <div style={{ margin: '1em' }}>
-      <Counter />
       <People />
     </div>
   );
